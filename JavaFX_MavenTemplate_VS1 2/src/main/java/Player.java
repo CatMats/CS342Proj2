8a4bfd707e320b5461299d73f0d8a825390e5919
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-10-24 14:07:52 -0500

    feat: Player()

---
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/Player.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class Player {
+	ArrayList<Card> hand;
+	int anteBet;
+	int playBet;
+	int pairPlusBet;
+	int totalWinnings;
+	
+	Player() {
+		hand = new ArrayList<Card>(3);
+		anteBet = 0;
+		playBet = 0;
+		pairPlusBet = 0;
+		totalWinnings = 0;
+	}
+} 
