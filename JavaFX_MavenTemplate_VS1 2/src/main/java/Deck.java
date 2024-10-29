807cb32b3f70b18311b5af56eb118ffcd579bef2
Mateo Castillo <mcast29@uic.edu>
2024-10-29 11:57:39 -0500

    fixed Deck.java
    Added breaks to switch case on createDeck() and removed previous arrayList of card because class already extends to one, making it an ArrayList object itself.

--- JavaFX_MavenTemplate_VS1 2/src/main/java/Deck.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/Deck.java
@@ -1,48 +1,47 @@
 import java.util.ArrayList;
-import java.io.*; 
-import java.util.*; 
+import java.util.Collections;
 @SuppressWarnings("serial")
 public class Deck extends ArrayList<Card>{
-	public ArrayList<Card> cardList;
 	public Deck() {
 		this.createDeck();
 	}
 	
 	// Creates a Deck and places it into the ArrayList
 	public void createDeck() {
-		for(int i = 0; i < 3; i++) {
-			for(int j = 1; j < 15; j++) {
+		for(int i = 0; i < 4; i++) {
+			for(int j = 1; j < 14; j++) {
 				char letter = ' ';
 				switch (i) {
-				case 0:letter = 'C';
-				case 1:letter = 'D';
-				case 2:letter = 'S';
-				case 3:letter = 'H';
+				case 0: letter = 'C'; break;
+				case 1: letter = 'D'; break;
+				case 2: letter = 'S'; break;
+				case 3: letter = 'H'; break;
 				}
-				cardList.add(new Card(letter, j));
+				System.out.println(letter);
+				add(new Card(letter, j));
 			}
 		}
-		Collections.shuffle(cardList); 
+		Collections.shuffle(this); 
 	}
 	
 	// Clears the deck and gets a new deck
 	public void newDeck() {
-		cardList.clear();
+		clear();
 		this.createDeck();
 		
 	}
 	// Gives the top card of the deck
 	public Card takeCardFromDeck() {
-		return cardList.remove(0);
+		return remove(0);
 	}
 	// prints out the contents of a deck
 	public void printOutDeck() {
-		for (Card c: cardList) {
+		for (Card c: this) {
 			System.out.println(c.getSuit() + ":" + c.getValue());
 		}
 	}
 	// Returns the size of the deck
 	public int getDeckSize() {
-		return this.cardList.size();
+		return this.size();
 	}
 }
