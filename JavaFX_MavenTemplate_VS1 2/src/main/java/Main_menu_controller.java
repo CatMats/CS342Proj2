f9ab3b34698db894ced3b4148bdd66b9eceac951
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-10-29 18:24:25 -0500

    add: main_menu_controller()

---
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/Main_menu_controller.java
@@ -0,0 +1,24 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main_menu_controller {
+    @FXML
+    public void startGame(ActionEvent event) throws IOException {
+
+    }
+    @FXML
+    public void quitGame(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/quit_popup.fxml"));
+        Parent root = loader.load();
+        Main_menu_controller myctr = loader.getController();
+        Stage substage = new Stage();
+        substage.setTitle("Do you want to quit?");
+
+        //((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
+}
