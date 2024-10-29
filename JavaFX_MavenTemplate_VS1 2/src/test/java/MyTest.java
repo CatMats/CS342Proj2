a9890625275f51adb7a419b6d33dff2a8ed1255c
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-10-29 14:22:50 -0500

    test: new_deck test()

--- JavaFX_MavenTemplate_VS1 2/src/test/java/MyTest.java
+++ JavaFX_MavenTemplate_VS1 2/src/test/java/MyTest.java
@@ -19,10 +19,20 @@ class MyTest {
 	
 	@Test
 	void deck_test() {
-		myDeck.printOutDeck();
-		assertEquals(myDeck.get(0).getValue() == myDeck.takeCardFromDeck().getValue(), "Values of the head and removed card aren't equal");
-		assertEquals(myDeck.get(0).getValue() == myDeck.takeCardFromDeck().getValue(), "Values of the head and removed card aren't equal");
-		assertEquals(myDeck.get(0).getValue() == myDeck.takeCardFromDeck().getValue(), "Values of the head and removed card aren't equal");
+		//myDeck.printOutDeck();
+		assertEquals(myDeck.get(0).getValue(), myDeck.takeCardFromDeck().getValue(), "Values of the head and removed card aren't equal");
+		assertEquals(myDeck.get(0).getValue(), myDeck.takeCardFromDeck().getValue(), "Values of the head and removed card aren't equal");
+		assertEquals(myDeck.get(0).getValue(), myDeck.takeCardFromDeck().getValue(), "Values of the head and removed card aren't equal");
+	}
+	
+	@Test
+	void new_deck_test() {
+		Card topCard = myDeck.get(0);
+		myDeck.newDeck();
+		// This could hypothetically fail if we somehow shuffle and have the same exact card on top twice in a row, we simply won't.
+		assertNotEquals(topCard.value, myDeck.takeCardFromDeck().value, "The cards on the top are equal!");
+		assertNotEquals(topCard.suit, myDeck.takeCardFromDeck().suit, "The cards on the top are equal!");
+		assertNotEquals(topCard, myDeck.takeCardFromDeck(), "The cards on the top are equal!");
 	}
 
 }
