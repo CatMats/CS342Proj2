3555de48dc89b723eed3f33ffd41b0a5f8e0319a
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-10-31 23:20:41 -0500

    feat: quitGame() button

--- JavaFX_MavenTemplate_VS1 2/src/main/java/Main_menu_controller.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/Main_menu_controller.java
@@ -2,13 +2,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Main_menu_controller {
     @FXML
@@ -27,12 +30,11 @@ public class Main_menu_controller {
     }
     @FXML
     public void quitGame(ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/quit_popup.fxml"));
-        Parent root = loader.load();
-        Main_menu_controller myctr = loader.getController();
-        Stage substage = new Stage();
-        substage.setTitle("Do you want to quit?");
-
-        //((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+        Alert alert = new Alert(Alert.AlertType.NONE, null, ButtonType.YES, ButtonType.NO);
+        alert.setTitle("Do You want to quit?");
+        alert.setHeaderText("Are you sure you want to quit?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.NO) {alert.close();}
+        else if (result.get() == ButtonType.YES) {((Stage)(((Button)event.getSource()).getScene().getWindow())).close();}
     }
 }
