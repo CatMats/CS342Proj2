7d029276b907f19ed416afea4806695c7218748e
Mateo Castillo <mcast29@uic.edu>
2024-11-03 18:13:02 -0600

    feat: Added more test cases for both the Card and Dealer class

--- JavaFX_MavenTemplate_VS1 2/src/test/java/MyTest.java
+++ JavaFX_MavenTemplate_VS1 2/src/test/java/MyTest.java
@@ -9,14 +9,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+
 class MyTest {
 		Deck myDeck;
+		Dealer theDealer;
+		ArrayList<Card> myHand;
 	@BeforeEach
 	void setup() {
 		myDeck = new Deck();
+		theDealer = new Dealer();
+		myHand = new ArrayList<>();
 	}
 	
-	
+
+	@Test
+	void deck_exists(){
+		assertNotNull(myDeck);
+	}
+
+
 	@Test
 	void deck_test() {
 		//myDeck.printOutDeck();
@@ -35,4 +48,33 @@ class MyTest {
 		assertNotEquals(topCard, myDeck.takeCardFromDeck(), "The cards on the top are equal!");
 	}
 
+	@Test
+	void dealer_exists(){
+		assertNotNull(theDealer, "Dealer object must be initialized");
+	}
+
+	@Test
+	void dealer_deck_exists(){
+		assertNotNull(theDealer.theDeck, "Dealer must have a deck.");
+		assertEquals(theDealer.theDeck.size(), 52, "Deck must have  size 52 at initialization");
+	}
+
+	@Test
+	void dealer_Deals(){
+		theDealer.dealersHand = theDealer.dealHand();
+
+		assertEquals(theDealer.dealersHand.size(), 3);
+		assertEquals(theDealer.theDeck.size(), 49);
+	}
+
+	@Test
+	void dealer_deals_deckRefresh(){
+		theDealer.dealersHand = theDealer.dealHand();
+		Player thePlayer = new Player();
+		for(int i = 0; i < 6; i++){
+			thePlayer.hand = theDealer.dealHand();
+			thePlayer.hand.clear();
+		}
+		assertEquals(theDealer.theDeck.size(), 49);
+	}
 }
