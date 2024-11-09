1e5782f0c672d3143f0f5661edd95763c8901ec2
Mateo Castillo <mcast29@uic.edu>
2024-11-09 16:24:18 -0600

    updated various classes

--- JavaFX_MavenTemplate_VS1 2/src/main/java/Dealer.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/Dealer.java
@@ -7,7 +7,7 @@ public class Dealer {
 		theDeck = new Deck();
 	}
 
-
+	//takes 3 cards from theDeck and gives them to players and dealer.
 	public ArrayList<Card> dealHand() {
 		ArrayList<Card> dealtCards = new ArrayList<>();
 		for(int i = 0; i < 3; i++) {dealtCards.add(theDeck.takeCardFromDeck());}
